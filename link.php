e4c6e7797668002411e3b7075fb04fa6818b8228
Ashishbindapi <ashish.k@bindapi.com>
2023-02-04 08:53:32 +0530

    init

--- link.php
+++ link.php
@@ -4,3 +4,5 @@ echo 'PASSWORD => 28fSbMyPTDwCbq5';
 echo 'https://mbt.bindapi.com/my_view_page.php';
 echo 'http://localhost/phpmyadmin/';
 echo 'https://chat.openai.com/chat';
+echo  'ashish.k@bindapi.com';
+echo  'Dev7-!@36';
