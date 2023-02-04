58977f116f499ee4558f116514712f16e6d40805
Ashishbindapi <ashish.k@bindapi.com>
2023-02-04 08:57:09 +0530

    init

--- link.php
+++ link.php
@@ -4,6 +4,7 @@ echo  'Dev7-!@36';
 
 echo 'User => DevBindApiAccessToken';
 echo 'PASSWORD => 28fSbMyPTDwCbq5';
+
 echo  'https://1drv.ms/b/s!Annieo2RWZiLg6lzGKcc2YCKPQvkSw';
 echo 'https://mbt.bindapi.com/my_view_page.php';
 echo 'http://localhost/phpmyadmin/';
