7f5851207783ec1054ac6fccb062d721ce264d24
Ashishbindapi <ashish.k@bindapi.com>
2023-02-04 08:49:52 +0530

    first commit

---
+++ link.php
@@ -0,0 +1,6 @@
+<?php
+echo 'User => DevBindApiAccessToken';
+echo 'PASSWORD => 28fSbMyPTDwCbq5';
+echo 'https://mbt.bindapi.com/my_view_page.php';
+echo 'http://localhost/phpmyadmin/';
+echo 'https://chat.openai.com/chat';
